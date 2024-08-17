80f9185b2d2c2325ae7f0ed2908e5c88ec4cfe86
Z3P0L <worldtico@gmail.com>
2024-08-17 17:20:48 -0500

    remove unnecessary code

--- exercises/flexbox-boxes/components/styles.js
+++ exercises/flexbox-boxes/components/styles.js
@@ -1,6 +1,4 @@
-import { StyleSheet, Dimensions } from 'react-native';
-
-const { width } = Dimensions.get('window');
+import { StyleSheet } from 'react-native';
 
 const commonStyles = StyleSheet.create({
 	container: {
