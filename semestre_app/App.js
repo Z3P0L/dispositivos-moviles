8b2b0efcd35facab91d1728d4bc00a5c76136291
Z3P0L <worldtico@gmail.com>
2024-08-24 08:04:08 -0500

    update

--- semestre_app/App.js
+++ semestre_app/App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+  const [api_record, setApiRecord] = useState();
+
+  const update_count = (number) => {
+    setCount(count + number);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Count: {count}</Text>
+      <Button onPress={() => update_count(1)} title="Sumar"></Button>
+      <Button onPress={() => update_count(-1)} title="Restar"></Button>
       <StatusBar style="auto" />
     </View>
   );
