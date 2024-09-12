a2abc08c73368e5efa5fd9107fc1233bceba5bc6
Z3P0L <worldtico@gmail.com>
2024-09-12 09:24:47 -0500

    Refactor: move app state management to Navigation component

--- semestre_app/App.js
+++ semestre_app/App.js
@@ -1,32 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, FlatList } from 'react-native';
 import { useState } from 'react';
+import Navigation from './src/components/navigation';
 
 export default function App() {
-
-  const [count, setCount] = useState(0);
-  const [api_record, setApiRecord] = useState();
-
-  const update_count = (number) => {
-    setCount(count + number);
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Count: {count}</Text>
-      <Button onPress={() => update_count(1)} title="Sumar"></Button>
-      <Button onPress={() => update_count(-1)} title="Restar"></Button>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
